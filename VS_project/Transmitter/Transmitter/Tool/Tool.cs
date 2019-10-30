fab48086482778817754c647060a64b8e11a7ca0
DESKTOP-33FM5VK\polu6 <polu635241@gmail.com>
2019-10-31 01:31:27 +0800

    新增工具類

---
+++ VS_project/Transmitter/Transmitter/Tool/Tool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transmitter.Tool
+{
+    public static class Tool
+    {
+        public static bool CheckAdd<T>(this List<T> m_List, T input)
+        {
+            if (!m_List.Exists(item => item.Equals(input)))
+            {
+                m_List.Add(input);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
