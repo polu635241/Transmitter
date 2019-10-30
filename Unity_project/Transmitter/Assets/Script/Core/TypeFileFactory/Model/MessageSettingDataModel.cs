dc6a16ab7c4b83bb02807fff3db39a1a03e9ca41
Polu <polu635241@gmail.com>
2019-10-31 03:44:29 +0800

    新增 封包 生成檔的 資料結構設定檔

---
+++ Unity_project/Transmitter/Assets/Script/Core/TypeFileFactory/Model/MessageSettingDataModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Transmitter.TypeSettingDataFactory.Model
+{
+	[Serializable]
+	public class EnumSettingData
+	{
+		public string enumName;
+		public List<string> items;
+	}
+
+	[Serializable]
+	public class TypeSettingData
+	{
+		public string typeName;
+
+		public List<FieldSettingData> fieldDatas;
+	}
+
+	[Serializable]
+	public class FieldSettingData
+	{
+		public string fieldName;
+
+		public string typeName;
+
+		public FieldAttribute fieldAttribute;
+	}
+
+	public enum FieldAttribute:int
+	{
+		singal=0,
+		array = 1,
+		list =2
+	}
+}
