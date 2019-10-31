5f2868be9c818af65254244b46a8198150522c46
Polu <polu635241@gmail.com>
2019-11-01 03:23:18 +0800

    編寫封包的基礎型別

---
+++ Unity_project/Transmitter/Assets/Script/Core/Message/BaseType/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Transmitter.Serialize
+{
+	[Serializable]
+	public abstract class Message {
+
+		public abstract byte[] GetByteArray ();
+
+		public abstract void Init (byte[] msg);
+
+		protected bool hasInit;
+	}
+	
+}
