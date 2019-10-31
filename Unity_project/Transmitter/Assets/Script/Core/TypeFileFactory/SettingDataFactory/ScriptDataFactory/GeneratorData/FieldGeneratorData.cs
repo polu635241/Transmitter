b5e7ce4b31790578fcf5526c0b9d5c34ab1e21e0
Polu <polu635241@gmail.com>
2019-11-01 03:31:57 +0800

    命名空間層級 變數層級 以及Enum層級生成檔

---
+++ Unity_project/Transmitter/Assets/Script/Core/TypeFileFactory/SettingDataFactory/ScriptDataFactory/GeneratorData/FieldGeneratorData.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Transmitter.TypeSettingDataFactory.Model
+{
+	public class FieldGeneratorData:GeneratorData
+	{
+		FieldSettingData fieldSettingData;
+
+		public FieldGeneratorData(FieldSettingData fieldSettingData):base(null)
+		{
+			this.fieldSettingData = fieldSettingData;
+		}
+
+		protected override void GeneratorContent ()
+		{
+			string origanFieldType = fieldSettingData.typeName;
+			string processFieldType = "";
+
+			switch(fieldSettingData.fieldAttribute)
+			{
+			case FieldAttribute.singal:
+				{
+					processFieldType = origanFieldType;
+					break;
+				}
+
+			case FieldAttribute.array:
+				{
+					processFieldType = origanFieldType+"[]";
+					break;
+				}
+
+			case FieldAttribute.list:
+				{
+					processFieldType = string.Format("List<{0}>",origanFieldType);
+					break;
+				}
+			}
+
+			string formatFiledLine = string.Format ("public {0} {1};", processFieldType, fieldSettingData.fieldName);
+
+			ProcessAddLine (formatFiledLine);
+		}
+			
+		protected override bool ExitHook 
+		{
+			get 
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ Unity_project/Transmitter/Assets/Script/Core/TypeFileFactory/SettingDataFactory/ScriptDataFactory/GeneratorData/NamespaceGeneratorData.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Transmitter.TypeSettingDataFactory.Model
+{
+	public class NamespaceGeneratorData:GeneratorData
+	{
+		List<string> usingNamespaces = new List<string>();
+		string belongNamespace = "";
+
+		public NamespaceGeneratorData (List<string> usingNamespaces, string belongNamespace, List<GeneratorData> nodes) : base (nodes)
+		{
+			this.usingNamespaces = usingNamespaces;
+			this.belongNamespace = belongNamespace;
+		}
+
+		protected override void GeneratorContent ()
+		{
+			//加上引用的空間
+			usingNamespaces.ForEach (_namespace => 
+				{
+					string processUsingLine = GetMixNamespace(_namespace);
+					ProcessAddLine(processUsingLine);
+				});
+			AddTempLine ();
+			ProcessAddLine (@"//" + declareMessage);
+			string belongNamespaceStr = "namespace" + GetSpace () + belongNamespace;
+
+			ProcessAddLine (belongNamespaceStr);
+			ProcessAddLine ("{");
+		}
+
+
+		string GetMixNamespace(string usingNamespace)
+		{
+			return string.Format ("using{0}{1};", GetSpace (), usingNamespace);
+		}
+	}
+}
