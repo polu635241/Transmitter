cb2d9538f82943655684884f6a6007b8ef43075d
Polu <polu635241@gmail.com>
2019-11-01 03:24:18 +0800

    生成 class層級用結構 生成 class 對應設定檔

---
+++ Unity_project/Transmitter/Assets/Script/Core/TypeFileFactory/SettingDataFactory/ScriptDataFactory/GeneratorData/ClassGeneratorData.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Transmitter.TypeSettingDataFactory.Model
+{
+	public class ClassGeneratorData:GeneratorData
+	{
+		GeneratorClassSetting generatorClassSetting;
+
+		public ClassGeneratorData (GeneratorClassSetting drawClassSetting, List<GeneratorData> nodes) : base (nodes)
+		{
+			this.generatorClassSetting = drawClassSetting;
+		}
+
+		protected override void GeneratorContent ()
+		{
+//			string formatClassName = string.Format ("public partial class {0} :Message", typeName);
+
+			string drawPartial = generatorClassSetting.isPartial ? " partial " : " ";
+			string drawClass = generatorClassSetting.className;
+			string drawInherit = generatorClassSetting.HasInherit ? (" : " + generatorClassSetting.InheritName) : "";
+
+			string formatClassName = string.Format ("public{0}class {1}{2}", drawPartial, drawClass, drawInherit);
+
+			if (generatorClassSetting.drawSerializableAttribute) 
+			{
+				ProcessAddLine ("[System.Serializable]");
+			}
+
+			ProcessAddLine (formatClassName);
+			ProcessAddLine ("{");
+		}
+	}
+}
---
+++ Unity_project/Transmitter/Assets/Script/Core/TypeFileFactory/SettingDataFactory/ScriptDataFactory/GeneratorData/SettingData/GeneratorClassSetting.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Transmitter.TypeSettingDataFactory.Model
+{
+	[System.Serializable]
+	public class GeneratorClassSetting
+	{
+		public bool isPartial = false;
+
+		public bool drawSerializableAttribute;
+
+		public string className;
+
+		bool hasInherit = false;
+		public bool HasInherit
+		{
+			get
+			{
+				return hasInherit;
+			}
+		}
+
+		string inheritName;
+		public string InheritName
+		{
+			get
+			{
+				return inheritName;
+			}
+
+			set
+			{
+				inheritName = value;
+
+				hasInherit = true;
+			}
+		}
+
+	}	
+}
