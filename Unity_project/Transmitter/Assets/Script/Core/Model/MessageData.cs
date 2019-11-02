236f4d8ac753b10551dcc2eb9d13923d3c606f96
Polu <polu635241@gmail.com>
2019-11-03 02:51:14 +0800

    新增MessageData 內部作為格式化之用 由外部傳入將單一object與byte[] 互相的轉換方法

---
+++ Unity_project/Transmitter/Assets/Script/Core/Model/MessageData.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Transmitter.Net.Model
+{
+	[Serializable]
+	public class MessageData
+	{
+		public string channelName;
+		public string eventName;
+		public object[] objs;
+
+		#region 合成公式
+		// int (頻道長度)
+		// string (頻道)
+
+		// int (事件長度)
+		// string (事件)
+
+		// int (有幾個參數)
+
+		// Loop :
+		// 參數型態
+		// 參數長度
+		// 參數
+		#endregion
+
+		/// <summary>
+		/// 透過實體物件建構
+		/// </summary>
+		public static MessageData CreateByDefaultFormat(string channelName,string eventName,params object[] objs)
+		{
+			MessageData messageData = new MessageData ();
+			
+			messageData.channelName = channelName;
+			messageData.eventName = eventName;
+			messageData.objs = objs;
+
+			return messageData;
+		}
+
+		/// <summary>
+		/// 內部作為格式化之用 由外部傳入將單一object轉換成byte[]方法
+		/// </summary>
+		/// <returns>The buffer.</returns>
+		/// <param name="serializeObjToBuffer">Serialize object to buffer.</param>
+		public byte[] GetBuffer(Func<string,object,byte[]> serializeObjToBuffer)
+		{
+			byte[] buffer = null;
+			MemoryStream memoryStream = null;
+			BinaryWriter binaryWriter = null;
+
+			try
+			{
+				memoryStream = new MemoryStream();
+				binaryWriter = new BinaryWriter(memoryStream);
+
+				//寫入頻道名稱
+				byte[] channelbytes = Encoding.UTF8.GetBytes(channelName);
+				binaryWriter.Write((ushort)channelbytes.Length);
+				binaryWriter.Write(channelbytes);
+
+				//寫入事件名稱
+				byte[] eventbytes = Encoding.UTF8.GetBytes(eventName);
+				binaryWriter.Write((ushort)eventbytes.Length);
+				binaryWriter.Write(eventbytes);
+
+				if(objs==null)
+				{
+					binaryWriter.Write((ushort)0);
+				}
+				else
+				{
+					binaryWriter.Write((ushort)objs.Length);
+
+					for (int i = 0; i < objs.Length; i++) 
+					{
+						object _object = objs[i];
+						string fullName = _object.GetType().FullName;
+						binaryWriter.Write(fullName);
+						byte[] objBuffer = serializeObjToBuffer(fullName, objs[i]);
+						binaryWriter.Write((ushort)objBuffer.Length);
+						binaryWriter.Write(objBuffer);
+					}
+				}
+
+				binaryWriter.Flush();
+				buffer = memoryStream.ToArray();
+
+			}
+			catch (Exception e) 
+			{
+				Debug.LogError (e.Message);
+			}
+			finally
+			{
+				memoryStream?.Dispose ();
+				binaryWriter?.Dispose ();
+			}
+			return buffer;
+		}
+
+		/// <summary>
+		/// 透過封包建構 外部傳入將單一段byte[]轉換成object的方法 作為格式化的依據
+		/// </summary>
+		/// <param name="byteData">Byte data.</param>
+		public static MessageData CreateByMsg(Func<string,byte[],object> deserializeToObject ,byte[] byteData)
+		{
+			MessageData messageData = new MessageData ();
+			
+			MemoryStream memoryStream = null;
+			BinaryReader binaryReader = null;
+
+			try
+			{
+				memoryStream = new MemoryStream(byteData);
+				binaryReader = new BinaryReader(memoryStream);
+
+				ushort channelNameLen = binaryReader.ReadUInt16 ();
+				byte[] buffer = binaryReader.ReadBytes(channelNameLen);
+				messageData.channelName = Encoding.UTF8.GetString (buffer);
+
+				ushort eventNameLen = binaryReader.ReadUInt16 ();
+				buffer = binaryReader.ReadBytes (eventNameLen);
+				messageData.eventName = Encoding.UTF8.GetString (buffer);
+
+				ushort parsCount = binaryReader.ReadUInt16 ();
+
+				if (parsCount > 0) 
+				{
+					List<object> parTable = new List<object> ();
+
+					for (int i = 0; i < parsCount; i++) 
+					{
+						string objectTypeFullName = binaryReader.ReadString();
+						ushort parLen = binaryReader.ReadUInt16 ();
+						byte[] parBuffer = binaryReader.ReadBytes (parLen);
+
+						object obj = deserializeToObject(objectTypeFullName,parBuffer);
+
+						parTable.Add(obj);
+					}
+
+					messageData.objs = parTable.ToArray ();
+				}
+			}
+			catch(Exception e) 
+			{
+				Debug.LogError (e.Message);
+			}
+			finally
+			{
+				memoryStream?.Dispose ();
+				binaryReader?.Dispose ();
+			}
+
+			return messageData;
+		}
+	}	
+}
