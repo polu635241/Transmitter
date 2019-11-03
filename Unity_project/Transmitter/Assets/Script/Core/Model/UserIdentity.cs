660e4cbb99d444fe780ff0d63fe01f53f408e1cb
Polu <polu635241@gmail.com>
2019-11-04 00:07:19 +0800

    新增玩家身分的基本資訊模型

---
+++ Unity_project/Transmitter/Assets/Script/Core/Model/UserIdentity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Transmitter.Net.Model
+{
+	public class UserIdentity{
+		public string IP;
+		public string Token;
+	}
+}
