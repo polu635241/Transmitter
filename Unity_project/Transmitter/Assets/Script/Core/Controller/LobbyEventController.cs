4650f35a63abb69c93fedca7e2f40188cba232df
Polu <polu635241@gmail.com>
2019-11-04 00:07:38 +0800

    新增其餘玩家的移入移出事件以及 成功連上伺服器事件

---
+++ Unity_project/Transmitter/Assets/Script/Core/Controller/LobbyEventController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Transmitter.Net.Model
+{
+	public class LobbyEventData
+	{
+		/// <summary>
+		/// 進入大廳時會回傳先前已經進入者的 UserData
+		/// </summary>
+		event Action<List<UserIdentity>> OnJoinLobby;
+
+		/// <summary>
+		/// 進入大廳時會回傳先前已經進入者的 UserData
+		/// </summary>
+		public void RegeistedOnJoinLobby(Action<List<UserIdentity>> onJoinLobby)
+		{
+			this.OnJoinLobby += onJoinLobby;
+		}
+
+		/// <summary>
+		/// 進入大廳時會回傳先前已經進入者的 UserData
+		/// </summary>
+		event Action<UserIdentity> OnUserAdd;
+
+		/// <summary>
+		/// 進入大廳時會回傳先前已經進入者的 UserData
+		/// </summary>
+		public void RegeistedOnUserAdd(Action<UserIdentity> onUserAdd)
+		{
+			this.OnUserAdd += onUserAdd;
+		}
+
+		/// <summary>
+		/// 進入大廳時會回傳先前已經進入者的 UserData
+		/// </summary>
+		event Action<UserIdentity> OnUserRemove;
+
+		/// <summary>
+		/// 進入大廳時會回傳先前已經進入者的 UserData
+		/// </summary>
+		public void RegeistedOnUserRemove(Action<UserIdentity> onUserRemove)
+		{
+			this.OnUserRemove += onUserRemove;
+		}
+	}	
+}
