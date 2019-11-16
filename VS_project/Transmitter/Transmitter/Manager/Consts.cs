d3cc3fed92b210f505310ab42108070bfa50972e
Polu <polu635241@gmail.com>
2019-11-16 20:28:04 +0800

    新增檔頭設定檔 後續client與server的溝通 也透過檔頭判斷是 遊戲間交互訊息 還是 lobby交互訊息

---
+++ VS_project/Transmitter/Transmitter/Manager/Consts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transmitter.DataStruct
+{
+    public static class Consts
+    {
+        public static class NetworkEvents
+        {
+            public const short GameMessage = 1001;
+            public const short AddUser = 1002;
+            public const short RemoveUser = 1003;
+
+
+            public const short NewUserReq = 1004;
+            public const short NewUserRes = 1004;
+        }
+    }
+}
