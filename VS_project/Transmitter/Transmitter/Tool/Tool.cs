f7e7f46b3b9995b368e7261b6fcb7f94020022fc
Polu <polu635241@gmail.com>
2019-11-17 02:48:44 +0800

    新增參考型keyValuePair結構 新增 擴展方法合併兩個Array

--- VS_project/Transmitter/Transmitter/Tool/Tool.cs
+++ VS_project/Transmitter/Transmitter/Tool/Tool.cs
@@ -8,6 +8,11 @@ namespace Transmitter.Tool
 {
     public static class Tool
     {
+        public static T[] Combine<T>(T[] arr1, T[] arr2)
+        {
+            return arr1.Concat(arr2).ToArray();
+        }
+
         public static bool CheckAdd<T>(this List<T> m_List, T input)
         {
             if (!m_List.Exists(item => item.Equals(input)))
@@ -26,6 +31,22 @@ namespace Transmitter.Tool
             {
                 callback.Invoke(item.Key, item.Value);
             }
+        }
+    }
+
+    /// <summary>
+	/// 本來的keyValuePair是 valueType 簡而言之就是refenceType
+	/// </summary>
+	[Serializable]
+    public class RefKeyValuePair<TKey, TValue>
+    {
+        public TKey key;
+        public TValue value;
+
+        public RefKeyValuePair(TKey key, TValue value)
+        {
+            this.key = key;
+            this.value = value;
         }
     }
 }
