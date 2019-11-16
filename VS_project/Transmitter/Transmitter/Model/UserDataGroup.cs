57c810a94513cc87594c49b43c6adfd88a63a312
Polu <polu635241@gmail.com>
2019-11-16 20:01:41 +0800

    修改結構名稱 並新增包含新玩家在內的資料結構

---
+++ VS_project/Transmitter/Transmitter/Model/UserDataGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transmitter.Model
+{
+    [Serializable]
+    public class UserDataGroup
+    {
+        /// <summary>
+        /// 現存玩家的資料
+        /// </summary>
+        public List<UserData> UserDatas;
+
+        /// <summary>
+        /// 新連線進來的玩家的udid
+        /// </summary>
+        public ushort NewUserUDID;
+    }
+}
