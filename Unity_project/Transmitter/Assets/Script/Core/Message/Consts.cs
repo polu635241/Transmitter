168744b548470eb816a0273fd9e5c66a260f1674
Polu <polu635241@gmail.com>
2019-11-19 02:34:11 +0800

    從VS端取得檔頭設定腳本

---
+++ Unity_project/Transmitter/Assets/Script/Core/Message/Consts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transmitter.DataStruct
+{
+    public static class Consts
+    {
+        public static class NetworkEvents
+        {
+            public const ushort GameMessage = 1001;
+            public const ushort AddUser = 1002;
+            public const ushort RemoveUser = 1003;
+
+
+            public const ushort NewUserReq = 1004;
+            public const ushort NewUserRes = 1004;
+        }
+    }
+}
