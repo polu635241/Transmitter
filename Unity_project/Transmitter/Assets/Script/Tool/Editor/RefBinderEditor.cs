5c9979ce1c173c946fa1143f54151ee7619b5953
Polu <polu635241@gmail.com>
2019-12-04 06:18:48 +0800

    修改Tool.EditorTool資料結名稱為 Tool.Editor 引入參考綁定器腳本

---
+++ Unity_project/Transmitter/Assets/Script/Tool/Editor/RefBinderEditor.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+namespace Transmitter.Tool
+{
+	[CustomEditor(typeof(RefBinder))]
+	public class RefBinderEditor : SerializedObjectEditor_FullOverride<RefBinder> {
+
+		const float newRefGatherFieldWidth = 125f;
+		const float buttonWidth = 125f;
+
+		const float keyWidth = 125f;
+		const float gameObjectWidth = 175f;
+
+		const string refGatherKeyFieldName = "key";
+		const string refGatherGOFieldDisplayName = "Gameobject";
+		const string refGatherGOFieldName = "go";
+
+		public override void OnInspectorGUI ()
+		{
+			base.OnInspectorGUI ();
+			
+			DrawRefGathers ();
+
+			serializedObject.ApplyModifiedProperties ();
+		}
+
+		void DrawRefGathers()
+		{
+			SerializedProperty refGathersProperty = serializedObject.FindProperty ("refGathers");
+
+			EditorGUILayout.LabelField ("RefGathers :", titleNameGUIStyle);
+
+			EditorGUI.indentLevel++;
+
+			DrawRefGatherSerlizedField (refGathersProperty);
+
+			EditorGUILayout.Space ();
+
+			DrawRefGatherAddFunction (refGathersProperty);
+			EditorGUI.indentLevel--;
+		}
+
+		void DrawRefGatherSerlizedField(SerializedProperty refGathersProperty)
+		{
+			EditorTool.DrawInVertical (()=>
+				{
+					int gatherSize = refGathersProperty.arraySize;
+					if (gatherSize > 0) 
+					{
+						List<int> cacheRemoves = new List<int> ();
+
+						EditorTool.DrawInHorizontal (()=>
+							{
+								EditorGUILayout.LabelField (refGatherKeyFieldName, GUILayout.Width (keyWidth));
+								EditorGUILayout.LabelField (refGatherGOFieldDisplayName, GUILayout.Width (gameObjectWidth));
+							});
+
+						for (int i = 0; i < gatherSize; i++) 
+						{
+							EditorTool.DrawInHorizontal (()=>
+								{
+									SerializedProperty itemProperty = refGathersProperty.GetArrayElementAtIndex (i);
+
+									bool hasRemove = DrawRefGatherItemAndGetRemove (itemProperty, refGathersProperty);
+
+									if (hasRemove) 
+									{
+										cacheRemoves.Add (i);
+									}
+								});
+						}
+
+
+						if (cacheRemoves.Count != 0) 
+						{
+							// 理論上一次的draw只能點一顆按鈕 所以list長度只會是1 
+							// 如果前面有移除過了 那應該要把移除後的對應index 再進行移除
+							// ex要把0跟1移除掉 改成呼叫兩次移除0
+							for (int i = 0; i < cacheRemoves.Count; i++) 
+							{
+								refGathersProperty.DeleteArrayElementAtIndex (cacheRemoves [i] - i);
+							}
+						}
+					}
+				});
+		}
+
+		bool DrawRefGatherItemAndGetRemove(SerializedProperty itemProperty,SerializedProperty rootProperty)
+		{
+			bool getRemove = false;
+
+			EditorTool.DrawInHorizontal (()=>
+				{
+					SerializedProperty keyProperty = itemProperty.FindPropertyRelative (refGatherKeyFieldName);
+					string originKey = keyProperty.stringValue;
+					string newKey = EditorGUILayout.DelayedTextField (originKey, GUILayout.Width (keyWidth));
+
+					if (newKey != originKey) 
+					{
+						int originSize = rootProperty.arraySize;
+
+						bool checkContinue = true;
+
+						for (int i = 0; i < originSize; i++) 
+						{
+							string otherItemKey = rootProperty.GetArrayElementAtIndex (i).FindPropertyRelative (refGatherKeyFieldName).stringValue;
+
+							if (otherItemKey == newKey) 
+							{
+								checkContinue = EditorUtility.DisplayDialog ("Error", "已存在相同key之物件 是否還是要改為此key", "是", "否");
+
+								break;
+							}
+						}
+
+						if (checkContinue) 
+						{
+							keyProperty.stringValue = newKey;
+						}
+					}
+
+					SerializedProperty goProperty = itemProperty.FindPropertyRelative (refGatherGOFieldName);
+					UnityEngine.Object originBindTarget = goProperty.objectReferenceValue;
+					UnityEngine.Object newBindTarget = EditorGUILayout.ObjectField (originBindTarget, typeof(GameObject), true, GUILayout.Width (gameObjectWidth));
+
+					if (newBindTarget != originBindTarget) 
+					{
+						goProperty.objectReferenceValue = newBindTarget;
+					}
+					if (GUILayout.Button ("Remove", buttonGUIStyle, GUILayout.MaxWidth (buttonWidth)))
+					{
+						getRemove = true;
+					}
+				},EditorTool.GUILayout.boxStyle
+			);
+
+			return getRemove;
+		}
+
+		void DrawRefGatherAddFunction(SerializedProperty refGathersProperty)
+		{
+			GUILayout.Space (classIntervalSpace);
+
+			EditorTool.DrawInHorizontal (()=>
+				{
+					EditorGUILayout.LabelField ("", GUILayout.Width (keyWidth));
+
+					newRefGatherGo = EditorGUILayout.ObjectField (newRefGatherGo, typeof(GameObject), true, GUILayout.Width (gameObjectWidth));
+
+
+					if (GUILayout.Button ("Add New Ref", buttonGUIStyle, GUILayout.MaxWidth (buttonWidth)))
+					{
+						if (newRefGatherGo != null) 
+						{
+							int originSize = refGathersProperty.arraySize;
+
+							string newItemkey = newRefGatherGo.name;
+
+							for (int i = 0; i < originSize; i++) 
+							{
+								string oldItemKey = refGathersProperty.GetArrayElementAtIndex (i).FindPropertyRelative (refGatherKeyFieldName).stringValue;
+								if (oldItemKey == newItemkey) 
+								{
+									bool checkProcessNewKey = EditorUtility.DisplayDialog ("Error", "新自動生成key和原有key相同 是否加上*號作為標記", "是", "否");
+
+									if (checkProcessNewKey) 
+									{
+										newItemkey = newItemkey.Insert (0, "*");
+									}
+									break;
+								}
+							}
+
+							refGathersProperty.InsertArrayElementAtIndex (originSize);
+							SerializedProperty newItem = refGathersProperty.GetArrayElementAtIndex (originSize);
+							newItem.FindPropertyRelative (refGatherKeyFieldName).stringValue = newItemkey;
+							newItem.FindPropertyRelative (refGatherGOFieldName).objectReferenceValue = newRefGatherGo;
+
+							newRefGatherGo = null;
+						}
+						else
+						{
+							EditorUtility.DisplayDialog ("Error", "欲寫入之物件為空", "ok");
+						}
+
+					}
+				});
+
+		}
+
+		UnityEngine.Object newRefGatherGo = null;
+	}
+}
---
+++ Unity_project/Transmitter/Assets/Script/Tool/RefBinder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Transmitter.Tool
+{
+	public class RefBinder : MonoBehaviour {
+
+		[Header("key Value pair table")]
+		[SerializeField]
+		List<RefGather> refGathers = new List<RefGather>();
+
+		public GameObject GetGameobject (string queyKey, bool throwException = true)
+		{
+			GameObject findTarget = null;
+			
+			RefGather finder = refGathers.Find (gather => gather.key == queyKey);
+
+			if (finder == null&&throwException) 
+			{
+				throw new Exception ("綁定不存在 可是綁定內容為空");
+			}
+			else
+			{
+				if (finder.go == null&&throwException)
+				{
+					throw new Exception ("綁定存在 可是綁定內容為空");
+				}
+				else
+				{
+					findTarget = finder.go;
+				}
+			}
+
+			return findTarget;
+		}
+	}
+
+	[Serializable]
+	class RefGather
+	{
+		public string key;
+		public GameObject go;
+	}
+}
