a32e8e11f3f4440a63698f150f6a7837cd38838b
Polu <polu635241@gmail.com>
2019-12-11 02:46:16 +0800

    新增玩家資料檔案 紀錄本地玩家UDID以及是否改過名字

---
+++ Unity_project/Transmitter/Assets/Demo/Script/NetworkPlayer/Model/NetworkPlayerData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Transmitter.Net;
+using Transmitter.Tool;
+
+namespace Transmitter.Demo
+{
+	public class NetworkPlayerData
+	{
+		public NetworkPlayerData(ushort udid)
+		{
+			this.udid = udid;
+		}
+		
+		public ushort Udid
+		{
+			get
+			{
+				return udid;
+			}
+		}
+
+		[SerializeField][ReadOnly]
+		ushort udid;
+
+		public bool HasModifyPlayerName
+		{
+			get
+			{
+				return hasModifyPlayerName;
+			}
+		}
+
+		[SerializeField][ReadOnly]
+		bool hasModifyPlayerName;
+
+		public string PlayerName
+		{
+			get
+			{
+				return playerName;
+			}
+
+			set
+			{
+				playerName = value;
+				hasModifyPlayerName = true;
+			}
+		}
+
+		[SerializeField][ReadOnly]
+		string playerName;
+	}
+}
