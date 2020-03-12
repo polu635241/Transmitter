8cd1d2b4c93a57daca26997fee42b8c8ff032c5b
Polu <polu635241@gmail.com>
2020-03-13 05:45:33 +0800

    整理視窗結構 並完成Enum的移除以及內容的修改實作

--- Unity_project/Transmitter/Assets/Script/MessageEditor/Editor/MessageBlackboard.cs
+++ Unity_project/Transmitter/Assets/Script/MessageEditor/Editor/MessageBlackboard.cs
@@ -1,4 +1,5 @@
 ﻿//#define keyMsg
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,7 +12,48 @@ namespace Transmitter.Tool
 	{
 		static MessageBlackboard instance;
 
+		Vector2 typeWindowSize = new Vector2 (300, 400);
+		Vector2 enumWindowSize = new Vector2 (300, 400);
+		const int GridInterval = 20;
+		const int BoundryGridInterval = 100;
+
 		const float moveSpeed = 0.5f;
+		const string NullBindDataMessage = "None.....";
+		const string NewEnumItem = "*";
+
+		GUIStyle EnumGUIStyle
+		{
+			get
+			{
+				if (enumGUIStyle == null) 
+				{
+					enumGUIStyle = new GUIStyle (EditorStyles.label);
+					enumGUIStyle.fontStyle = FontStyle.Bold;
+					enumGUIStyle.normal.textColor = Color.green;
+				}
+
+				return enumGUIStyle;
+			}
+		}
+
+		GUIStyle enumGUIStyle;
+
+		GUIStyle NoBindEnumGUIStyle
+		{
+			get
+			{
+				if (noBindEnumGUIStyle == null) 
+				{
+					noBindEnumGUIStyle = new GUIStyle (EditorStyles.label);
+					noBindEnumGUIStyle.fontStyle = FontStyle.Bold;
+					noBindEnumGUIStyle.normal.textColor = Color.red;
+				}
+
+				return noBindEnumGUIStyle;
+			}
+		}
+
+		GUIStyle noBindEnumGUIStyle;
 
 		[MenuItem("Transmitter/MessageBlackboard/Refresh _F2")]
 		static void Refresh ()
@@ -37,19 +79,31 @@ namespace Transmitter.Tool
 			{
 				MessageBlackboard messageBlackboard = GetWindow<MessageBlackboard> ("MessageBlackboard");
 				instance = messageBlackboard;
-				instance.RefreshSetting ();
 			}
 		}
 
-		List<RefKeyValuePair<Rect,TypeWindowSettingData>> typeWindowPairs = new List<RefKeyValuePair<Rect, TypeWindowSettingData>> ();
+		List<RefKeyValuePair<Rect,TypeWindowData>> typeWindowPairs = new List<RefKeyValuePair<Rect, TypeWindowData>> ();
+		List<string> enumNamesCache = new List<string> ();
+
+		List<RefKeyValuePair<Rect,EnumWindowData>> enumWindowPairs = new List<RefKeyValuePair<Rect, EnumWindowData>> ();
 		int typeWindowsCountCache;
-		List<RefKeyValuePair<Rect,EnumWindowSettingData>> enumWindowPairs = new List<RefKeyValuePair<Rect, EnumWindowSettingData>> ();
 
 		Vector2 mousePos_Mid;
 
 		MessageSettingCacheData messageSettingCacheData;
 
 		MessageSettingData messageSettingData;
+
+		List<TypeWindowData> waitRemoveTypeWindowDatas = new List<TypeWindowData> ();
+		List<EnumWindowData> waitRemoveEnumWindowDatas = new List<EnumWindowData> ();
+
+		List<TypeSettingData> waitDeleteTypeDatas = new List<TypeSettingData> ();
+		List<EnumSettingData> waitDeleteEnumDatas = new List<EnumSettingData> ();
+
+		void OnEnable ()
+		{
+			RefreshSetting ();
+		}
 
 		void OnGUI ()
 		{
@@ -60,7 +114,7 @@ namespace Transmitter.Tool
 
 			BeginWindows ();
 			{
-				typeWindowsCountCache = typeWindowPairs.Count;
+				ReFreshCache ();
 
 				typeWindowPairs.Map ((index,pair)=>
 					{
@@ -76,11 +130,24 @@ namespace Transmitter.Tool
 			EndWindows ();
 
 			ProcessEvent (currentEvent);
+			ProcessRemoveWindows ();
+			ProcessDeleteDatas ();
+		}
+
+		void ReFreshCache ()
+		{
+			typeWindowsCountCache = typeWindowPairs.Count;
+
+			enumNamesCache = new List<string> ();
+
+			messageSettingData.enumSettingDatas.ForEach (data => enumNamesCache.Add (data.enumName));
+
+			enumNamesCache.Add (NullBindDataMessage);
 		}
 
 		void DrawTypeWindow (int windowID)
 		{
-			TypeWindowSettingData typeWindowSettingData = GetTypeWindowSettingData (windowID);
+			TypeWindowData windowSettingData = GetTypeWindowSettingData (windowID);
 			
 			GUILayout.Button ("Type");
 			GUI.DragWindow ();
@@ -88,17 +155,181 @@ namespace Transmitter.Tool
 
 		void DrawEnumWindow (int windowID)
 		{
-			EnumWindowSettingData enumWindowSettingData = GetEnumWindowSettingData (windowID);
+			EnumWindowData windowSettingData = GetEnumWindowSettingData (windowID);
+			EnumSettingData bindEnumSettingData = windowSettingData.BindData;
 
-			GUILayout.Button ("Enum");
+			EditorGUILayout.Space ();
+
+			DrawSwitchBindModule (bindEnumSettingData, windowID);
+
+			EditorGUILayout.Space ();
+
+			//已經綁定完畢
+			if (bindEnumSettingData != null) 
+			{
+				DrawEnumItems (bindEnumSettingData);
+			}
+
+			//置底
+			GUILayout.FlexibleSpace ();
+
+			EditorTool.DrawInHorizontal (()=>
+				{
+					if (bindEnumSettingData != null) 
+					{
+						DrawDeleteEnum (windowSettingData);
+					}
+
+					DrawCloseEnum (windowSettingData);
+				});
+
 			GUI.DragWindow ();
+		}
+
+		void DrawDeleteEnum (EnumWindowData windowSettingData)
+		{
+			GUILayout.Space (3);
+
+			if(GUILayout.Button("Delete", GUILayout.Width (buttonWidth)))
+			{
+				if (RemoveDialogCondition.Invoke ())
+				{
+					waitDeleteEnumDatas.Add (windowSettingData.BindData);
+					waitRemoveEnumWindowDatas.Add (windowSettingData);
+				}	
+			}
+		}
+
+		void DrawCloseEnum (EnumWindowData windowSettingData)
+		{
+			GUILayout.FlexibleSpace ();
+
+			if(GUILayout.Button("Close", GUILayout.Width (buttonWidth)))
+			{
+				waitRemoveEnumWindowDatas.Add (windowSettingData);
+			}
+
+			GUILayout.Space (10);
+		}
+
+		void DrawSwitchBindModule (EnumSettingData bindEnumSettingData, int windowID)
+		{
+			bool hasBind = true;
+
+			string currentTitle;
+
+			if (bindEnumSettingData != null)
+			{
+				currentTitle = bindEnumSettingData.enumName;
+				hasBind = true;
+			}
+			else
+			{
+				currentTitle = NullBindDataMessage;
+				hasBind = false;
+			}
+
+			GUIStyle enumNameGUIStyle = hasBind ? EnumGUIStyle : NoBindEnumGUIStyle;
+
+			EditorTool.DrawInHorizontal (() =>
+			{
+				EditorGUILayout.LabelField (currentTitle, enumNameGUIStyle);
+
+				if (GUILayout.Button ("Switch", GUILayout.Width (buttonWidth)))
+				{
+					Action<string> switchBindData = (bindDataValue) =>
+					{
+						//可能會延遲執行 所以重新抓取
+						EnumWindowData _WindowSettingData = GetEnumWindowSettingData (windowID);
+						_WindowSettingData.BindData = GetEnumSettingData (bindDataValue);
+					};
+
+					Pop_UpSelectWindow.ShowWindow (switchBindData, currentTitle, enumNamesCache.ToArray ());
+				}
+
+				GUILayout.Space (10);
+			});
+		}
+
+		void DrawEnumItems (EnumSettingData bindEnumSettingData)
+		{
+			List<string> enumItems = bindEnumSettingData.items;
+
+			Action<int> onRemoveEnumItem = (removeIndex) => 
+			{
+				enumItems.RemoveAt (removeIndex);
+			};
+
+			Action<int> onCloneEnumItem = (cloneIndex) => 
+			{
+				string enumItem = enumItems [cloneIndex];
+				enumItems.Insert (cloneIndex + 1, enumItem);
+			};
+
+			RemoveAndCloneCache enumItemsRemoveAndCloneCache = new RemoveAndCloneCache (onRemoveEnumItem, onCloneEnumItem);
+
+			EditorTool.DrawInVertical (() => 
+				{
+					if (bindEnumSettingData != null) 
+					{
+						enumItems.Map ((index,enumValue)=>
+							{
+								DrawEnumItem (index, enumValue, bindEnumSettingData, enumItemsRemoveAndCloneCache);
+							});
+
+						enumItemsRemoveAndCloneCache.Flush ();
+
+						GUILayout.Space (10);
+
+						if(GUILayout.Button ("Add", GUILayout.Width (buttonWidth)))
+						{
+							enumItems.Add (NewEnumItem);
+						}
+					}
+				}, BoxGUIStyle);
+		}
+
+		void DrawEnumItem (int index, string enumValue, EnumSettingData bindEnumSettingData, RemoveAndCloneCache enumItemsRemoveAndCloneCache)
+		{
+			EditorGUILayout.Space ();
+			
+			EditorTool.DrawInHorizontal(()=>
+				{
+					//這裡取按鈕的寬度 是為了版型
+					string newEnumValue = EditorGUILayout.TextField (enumValue, GUILayout.Width (buttonWidth), GUILayout.Height (20));
+
+					GUILayout.FlexibleSpace ();
+
+					if(newEnumValue != enumValue)
+					{
+						bindEnumSettingData.items[index] = newEnumValue;
+						SaveToEntity ();
+					}
+
+					if(GUILayout.Button("Remove", GUILayout.Width (buttonWidth)))
+					{
+						if(RemoveDialogCondition.Invoke())
+						{
+							enumItemsRemoveAndCloneCache.RemoveIndex = index;
+						}
+					}
+
+					GUILayout.Space (10);
+
+					if(GUILayout.Button ("Clone", GUILayout.Width (buttonWidth)))
+					{
+						enumItemsRemoveAndCloneCache.CloneIndex = index;
+					}
+
+					GUILayout.Space (7);
+				});
 		}
 
 		void DrawGrids()
 		{
-			DrawGrid (20f, TranslucentBlack);
+			DrawGrid (GridInterval, TranslucentBlack);
 			//透過重疊 劃出較粗的外框線
-			DrawGrid (100f, TranslucentBlack);
+			DrawGrid (BoundryGridInterval, TranslucentBlack);
 		}
 
 		Vector2 windowDeltaPos;
@@ -158,6 +389,63 @@ namespace Transmitter.Tool
 			ProcessTempSpaceEvent (e);
 		}
 
+		void ProcessRemoveWindows ()
+		{
+			if (waitRemoveTypeWindowDatas.Count != 0) 
+			{
+				waitRemoveTypeWindowDatas.ForEach (data=>
+					{
+						typeWindowPairs.Remove (pair => pair.value== data);
+					});
+
+				waitRemoveTypeWindowDatas.Clear ();
+			}
+			
+			if (waitRemoveEnumWindowDatas.Count != 0) 
+			{
+				waitRemoveEnumWindowDatas.ForEach (data=>
+					{
+						enumWindowPairs.Remove (pair => pair.value== data);
+					});
+				
+				waitRemoveEnumWindowDatas.Clear ();
+			}
+		}
+
+		void ProcessDeleteDatas ()
+		{
+			bool needSaveEntity = false;
+			
+			if (waitDeleteTypeDatas.Count != 0) 
+			{
+				needSaveEntity = true;
+				
+				waitDeleteTypeDatas.ForEach (data=>
+					{
+						messageSettingData.typeSettingDatas.Remove (data);
+					});
+
+				waitDeleteTypeDatas.Clear ();
+			}
+
+			if (waitDeleteEnumDatas.Count != 0) 
+			{
+				needSaveEntity = true;
+				
+				waitDeleteEnumDatas.ForEach (data=>
+					{
+						messageSettingData.enumSettingDatas.Remove (data);
+					});
+
+				waitDeleteEnumDatas.Clear ();
+			}
+
+			if (needSaveEntity) 
+			{
+				SaveToEntity ();
+			}
+		}
+
 		/// <summary>
 		/// 當手點到空白處的事件
 		/// </summary>
@@ -211,18 +499,18 @@ namespace Transmitter.Tool
 
 		void CreateTypeWindow(Vector2 pos)
 		{
-			Rect windowRect = new Rect (pos.x, pos.y, 200, 200);
-			TypeWindowSettingData typeWindowSettingData = new TypeWindowSettingData ();
+			Rect windowRect = new Rect (pos.x, pos.y, typeWindowSize.x, typeWindowSize.y);
+			TypeWindowData typeWindowSettingData = new TypeWindowData ();
 
-			typeWindowPairs.Add (new RefKeyValuePair<Rect, TypeWindowSettingData> (windowRect, typeWindowSettingData));
+			typeWindowPairs.Add (new RefKeyValuePair<Rect, TypeWindowData> (windowRect, typeWindowSettingData));
 		}
 
 		void CreateEnumWindow (Vector2 pos)
 		{
-			Rect windowRect = new Rect (pos.x, pos.y, 200, 200);
-			EnumWindowSettingData enumWindowSettingData = new EnumWindowSettingData ();
+			Rect windowRect = new Rect (pos.x, pos.y, enumWindowSize.x, enumWindowSize.y);
+			EnumWindowData enumWindowSettingData = new EnumWindowData ();
 
-			enumWindowPairs.Add (new RefKeyValuePair<Rect, EnumWindowSettingData> (windowRect, enumWindowSettingData));
+			enumWindowPairs.Add (new RefKeyValuePair<Rect, EnumWindowData> (windowRect, enumWindowSettingData));
 		}
 
 		void TempSpaceMouseDragEvent (Event e)
@@ -279,11 +567,16 @@ namespace Transmitter.Tool
 			messageSettingCacheData = EditorTool.GetCacheData<MessageSettingCacheData> ();
 			messageSettingData = messageSettingCacheData.MessageSettingData;
 			windowDeltaPos = Vector2.zero;
-			typeWindowPairs = new List<RefKeyValuePair<Rect, TypeWindowSettingData>> ();
-			enumWindowPairs = new List<RefKeyValuePair<Rect, EnumWindowSettingData>> ();
+			typeWindowPairs = new List<RefKeyValuePair<Rect, TypeWindowData>> ();
+			enumWindowPairs = new List<RefKeyValuePair<Rect, EnumWindowData>> ();
 		}
 
-		TypeWindowSettingData GetTypeWindowSettingData (int selectIndex)
+		void SaveToEntity ()
+		{
+			EditorUtility.SetDirty(messageSettingCacheData);
+		}
+
+		TypeWindowData GetTypeWindowSettingData (int selectIndex)
 		{
 			if (selectIndex <= typeWindowsCountCache - 1) 
 			{
@@ -292,11 +585,11 @@ namespace Transmitter.Tool
 			else
 			{
 				Debug.LogError ($"index is out of range count -> {typeWindowsCountCache}, index -> {selectIndex}");
-				return new TypeWindowSettingData ();
+				return new TypeWindowData ();
 			}
 		}
 
-		EnumWindowSettingData GetEnumWindowSettingData (int selectIndex)
+		EnumWindowData GetEnumWindowSettingData (int selectIndex)
 		{
 			int processSelectIndex = selectIndex - typeWindowsCountCache;
 			int enumWindowsCount = enumWindowPairs.Count;
@@ -308,11 +601,28 @@ namespace Transmitter.Tool
 			else
 			{
 				Debug.LogError ($"index is out of range typeCount -> {typeWindowsCountCache}, enumCount -> {enumWindowsCount}, index -> {processSelectIndex}");
-				return new EnumWindowSettingData ();
+				return new EnumWindowData ();
 			}
 		}
 
-		private class TypeWindowSettingData
+		EnumSettingData GetEnumSettingData (string enumSettingDataName)
+		{	
+			EnumSettingData enumSettingData = messageSettingData.enumSettingDatas.Find (data => data.enumName == enumSettingDataName);
+
+			if (enumSettingData != null) 
+			{
+				return enumSettingData;
+			}
+			else
+			{
+				Debug.LogError ($"can't get EnumSettingData -> {enumSettingDataName}");
+				
+				return null;
+			}
+		}
+
+
+		private class TypeWindowData
 		{
 			public TypeSettingData BindData
 			{
@@ -325,17 +635,14 @@ namespace Transmitter.Tool
 			TypeSettingData bindData;
 		}
 
-		private class EnumWindowSettingData
+		private class EnumWindowData
 		{
-			public EnumSettingData BindData
+			public EnumWindowData ()
 			{
-				get
-				{
-					return bindData;
-				}
+				BindData = null;
 			}
 			
-			EnumSettingData bindData;
+			public EnumSettingData BindData{ get; set;}
 		}
 	}
 		
